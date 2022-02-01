ce9ff4e6520d32438e89994364f263acb41378a6
Zachary Kiihne <zkiihne@gmail.com>
2022-02-01 12:39:42 -0500

    Update global.js

--- Javascript/src/global.js
+++ Javascript/src/global.js
@@ -1,5 +1,5 @@
 
 var globals ={
-    etherscan_api_key: "FUE3N4UKZRRQNE4BND4SYCRHMPDY21M3TB",
-    key_contract_address: "0xDF6d7ADf09C6a168B6ED82928eE78a266B5E1182",
+    etherscan_api_key: "ETHSCAN API KEY HERE",
+    key_contract_address: "CONTRACT ADDRESS OF KEY CONTRACT",
 }
